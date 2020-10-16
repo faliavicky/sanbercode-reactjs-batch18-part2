7fc106c07e1390f4b2eaa8cc74ce0eabf2bb9174
Falia <faliavicky1128@gmail.com>
2020-10-16 07:47:34 +0700

    update tugas 14

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,8 +3,9 @@ import './App.css';
 /*import Tugas9 from './Tugas-9/tugas9';
 import Tugas10 from './Tugas-10/tugas10';
 import Tugas11 from './Tugas-11/tugas11';
-import Tugas12 from './Tugas-12/tugas12';*/
-import Tugas13 from './Tugas-13/tugas13';
+import Tugas12 from './Tugas-12/tugas12';
+import Tugas13 from './Tugas-13/tugas13';*/
+import Tugas14 from './Tugas-14/tugas14';
 
 function App() {
   return (
@@ -12,8 +13,9 @@ function App() {
      {/* < Tugas9 />
       < Tugas10 />
       < Tugas11 /> 
-     < Tugas12 />*/}
-     < Tugas13 />
+     < Tugas12 />
+     < Tugas13 />*/}
+     < Tugas14 />
     </div>
   );
 }
